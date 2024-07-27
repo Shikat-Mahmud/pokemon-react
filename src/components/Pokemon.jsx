09f8f97e41ad9cf5e90b0c291b7d6451f231dbb4
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-27 21:24:14 +0600

    fetched pokemon data from api | css added

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,5 +1,58 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 export const Pokemon = () => {
+    const [pokemon, setPokemon] = useState([]);
+
+    // api link
+    const API = "https://pokeapi.co/api/v2/pokemon?limit=24";
+
+    // fetch data from api
+    const fetchPokemon = async () => {
+        try {
+            const res = await fetch(API);
+
+            // store fetched data in json 
+            const data = await res.json();
+
+            // console.log(data);
+
+            const detailedPokemonData = data.results.map(async (curPokemon) => {
+                // console.log(curPokemon.url);
+
+                const res = await fetch(curPokemon.url);
+                const data = await res.json();
+
+                return data;
+            });
+
+            // console.log(detailedPokemonData);
+
+            const detailedResponse = await Promise.all(detailedPokemonData);
+            setPokemon(detailedResponse);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPokemon();
+    }, []);
+
     return (
-        <h1>Welcome Pokemon</h1>
+        <section className="container">
+            <header>
+                <h1>Welcome to Pokemon World</h1>
+            </header>
+            <div>
+                <ul className="cards">
+                    {
+                        pokemon.map((curPoke) => {
+                            return <li key={curPoke.id}>{curPoke.name}</li>
+                        })
+                    }
+                </ul>
+            </div>
+        </section>
     );
 };
